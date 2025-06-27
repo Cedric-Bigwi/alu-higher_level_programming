cedb5869992ebb47ba8b75ea515789ef3643dc31
Ced <c.bigwihin@alustudent.com>
2025-06-27 15:07:36 +0200

    Update 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+import sys
 from calculator_1 import add, sub, mul, div
-from sys import argv
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     a = int(sys.argv[1])
     operator = sys.argv[2]
@@ -21,6 +21,6 @@ if __name__ == "__main__":
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
