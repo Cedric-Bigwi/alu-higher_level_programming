92988d2700b8c3a200dd7c83e807ad00f4cf935d
Ced <c.bigwihin@alustudent.com>
2025-06-27 13:47:56 +0200

    Update 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print(*[chr(c).lower() if i % 2 == 0 else chr(c).upper() for i, c in enumerate(range(122, 96, -1))], sep="", end="")
+print("{}".format("".join([chr(i) if (122 - i) % 2 == 0 else chr(i - 32) for i in range(122, 96, -1)])), end="")
