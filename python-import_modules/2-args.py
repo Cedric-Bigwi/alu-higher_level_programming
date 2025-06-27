02a05db7be04469872cd7611c21ad7ee9e5fe054
Ced <c.bigwihin@alustudent.com>
2025-06-27 14:55:57 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    n = len(args)
+    if n == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(n, '' if n == 1 else 's'))
+        for i, arg in enumerate(args, 1):
+            print("{}: {}".format(i, arg))
