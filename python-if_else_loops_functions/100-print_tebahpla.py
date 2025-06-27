af060fa6e90e3e340fd5ab4fc0a5a784571a1562
Ced <c.bigwihin@alustudent.com>
2025-06-27 13:50:10 +0200

    Update 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-print("{}".format("".join([chr(i) if (122 - i) % 2 == 0 else chr(i - 32) for i in range(122, 96, -1)])), end="")
+print(
+    "{}".format(
+        "".join(
+            [
+                chr(i) if (122 - i) % 2 == 0 else chr(i - 32)
+                for i in range(122, 96, -1)
+            ]
+        )
+    ),
+    end=""
+)
