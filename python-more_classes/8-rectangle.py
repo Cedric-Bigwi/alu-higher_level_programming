e3d020db471c662d2ba0ab62150f62d210d8a3f2
Ced <c.bigwihin@alustudent.com>
2025-07-14 12:34:43 +0200

    Update 8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -9,7 +9,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initialize rectangle width and height, increment instance count."""
+        """Initialize rectangle width and height,
+        increment instance count."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
@@ -67,7 +68,8 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        """Print message when an instance is deleted, decrement instance count."""
+        """Print message when an instance is deleted,
+        decrement instance count."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
