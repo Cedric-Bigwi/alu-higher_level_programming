fa88539a3a74480a8b4b5aba75277eac8756e01a
Ced <c.bigwihin@alustudent.com>
2025-07-14 15:03:39 +0200

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 
-
-
 class Student:
-    """Defines a student with public attributes and optional filtering."""
+    """Defines a student with public attributes
+    and optional filtering."""
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -12,7 +11,9 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns dict of selected attributes if list is given, else all."""
+        """
+        Returns dict of selected attributes if list is given, else all.
+        """
         if isinstance(attrs, list):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
