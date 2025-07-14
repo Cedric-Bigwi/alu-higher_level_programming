ea843dd255ba046b1f35cd7323ebf3a92cef4d33
Ced <c.bigwihin@alustudent.com>
2025-07-14 12:33:59 +0200

    Update 6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        """Initialize rectangle width and height, 
+        """Initialize rectangle width and height,
         increment instance count."""
         self.width = width
         self.height = height
