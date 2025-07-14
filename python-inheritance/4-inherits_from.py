42c0a8dbf1bb26612063d8cff81a3003a7d584bf
Ced <c.bigwihin@alustudent.com>
2025-07-14 14:33:25 +0200

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module 4-inherits_from.py"""
+
+
 def inherits_from(obj, a_class):
     """Check if object inherits from class (not direct match)."""
     return isinstance(obj, a_class) and type(obj) is not a_class
