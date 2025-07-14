e4e4e3c07915db53cde96cdb546d3c00ab275362
Ced <c.bigwihin@alustudent.com>
2025-07-14 14:35:00 +0200

    Update 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,6 +2,7 @@
 """Module 9-rectangle.py"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle with area and str."""
     def __init__(self, width, height):
