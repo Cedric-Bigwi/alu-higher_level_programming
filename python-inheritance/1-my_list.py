335f53835392071f11763293552a0749eac31ada
Ced <c.bigwihin@alustudent.com>
2025-07-14 14:31:39 +0200

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-"""Module 1-my_list.py"""
+"""Creates a Mylist class that inherits from list."""
+
+
 class MyList(list):
-    """Custom list class with sorted print."""
+    """Mylist class => A sub class of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
+        """prints the sorted list"""
         print(sorted(self))
