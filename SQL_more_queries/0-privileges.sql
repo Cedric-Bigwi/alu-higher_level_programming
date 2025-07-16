9db126112cddc9b7bd4bf84a082dd7b4295e7249
Ced <c.bigwihin@alustudent.com>
2025-07-16 16:42:16 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Create user user_0d_1 with specific privileges
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
+-- List all privileges of user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
