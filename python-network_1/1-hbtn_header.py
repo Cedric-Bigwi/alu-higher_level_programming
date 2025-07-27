7b2c5d6fa841f42ea4ef14c5492881f624d9bfb7
Ced <c.bigwihin@alustudent.com>
2025-07-27 13:17:16 +0200

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,5 +2,7 @@
 """Displays the value of X-Request-Id from response header"""
 import urllib.request
 import sys
+"""This script fetches a given URL and displays a formatted response."""
+
 with urllib.request.urlopen(sys.argv[1]) as response:
     print(response.headers.get("X-Request-Id"))
