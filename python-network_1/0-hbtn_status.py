a31c2c575e1cdf982eb4337aad047f417e6b3fcb
Ced <c.bigwihin@alustudent.com>
2025-07-27 14:50:10 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,10 +4,9 @@
 from urllib import request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    with request.urlopen(url) as response:
+    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))'
