bb4ec573a1d81cc3654dc76cadbb64ff4f2ce192
Ced <c.bigwihin@alustudent.com>
2025-07-27 13:08:09 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Fetches http://0.0.0.0:5050/status and prints formatted response"""
 import urllib.request
-
 with urllib.request.urlopen("http://0.0.0.0:5050/status") as response:
     body = response.read()
     print("Body response:")
