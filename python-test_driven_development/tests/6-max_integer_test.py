8eeb43ed027fda759a15a5e22e768f4507c1a48c
Ced <c.bigwihin@alustudent.com>
2025-08-02 01:09:52 +0200

    Update 6-max_integer_test.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,51 @@
 #!/usr/bin/python3
-"""Unittest for max_integer function"""
+"""Unittest for max_integer([..])"""
+
 import unittest
-from 6-max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max(self):
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([10]), 10)
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([-5, -1, -8]), -1)
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_max_in_middle(self):
+        self.assertEqual(max_integer([1, 4, 2, 3]), 4)
+
+    def test_one_negative(self):
+        self.assertEqual(max_integer([1, -2, 3]), 3)
+
+    def test_all_negative(self):
+        self.assertEqual(max_integer([-1, -3, -2]), -1)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_duplicates(self):
+        self.assertEqual(max_integer([3, 3, 3]), 3)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.5, 2.7, 2.3]), 2.7)
+
+    def test_mixed_ints_and_floats(self):
+        self.assertEqual(max_integer([1, 2.5, 3]), 3)
+
+    def test_string_list(self):
+        self.assertEqual(max_integer(["a", "z", "b"]), "z")
+
+    def test_string_input(self):
+        self.assertEqual(max_integer("hello"), "o")
+
+    def test_none_input(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_non_iterable(self):
+        with self.assertRaises(TypeError):
+            max_integer(42)
