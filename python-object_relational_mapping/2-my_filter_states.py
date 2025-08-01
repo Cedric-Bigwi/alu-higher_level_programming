85fa50254aa1e7885d3bbf9da117f24204d0dd9e
Ced <c.bigwihin@alustudent.com>
2025-08-01 23:31:55 +0200

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
-"""List all states where name matches the argument"""
+"""
+Takes a state name as argument and displays all values in the states table
+where name matches the argument (case-sensitive match).
+Usage: ./2-my_filter_states.py <username> <password> <db_name> <state_name>
+"""
 
-import sys
 import MySQLdb
+import sys
+
+
+def main():
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=passwd,
+        db=db_name,
+        port=3306
+    )
+    cur = db.cursor()
+
+    # SQL injection-prone way required by the project (but with LIKE BINARY for case sensitivity)
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"\
+        .format(state_name)
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        exit()
-    user, passwd, db, name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db)
-    cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(name)
-    cur.execute(query)
-    for row in cur.fetchall():
-        print(row)
-    cur.close()
-    conn.close()
+    main()
