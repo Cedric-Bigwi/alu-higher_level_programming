e3ae14d0ffa75ab94a24161cf8e938eb33862234
Ced <c.bigwihin@alustudent.com>
2025-08-01 15:10:47 +0200

    Update 8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Sends a POST request to search for a user and displays the id and name or an error message."""
-
-import requests
+"""__summary__
+- writes a script that takes in a URL
+- sends a POST request to the URL with a letter as a parameter
+- and displays the JSON response
+"""
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
-    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        data = r.json()
-        if data:
-            print(f"[{data.get('id')}] {data.get('name')}")
-        else:
+        response = r.json()
+        if response == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
