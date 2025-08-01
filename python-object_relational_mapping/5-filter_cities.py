a52d3947a75d02877a8c6c3361a46907ea2a068c
Ced <c.bigwihin@alustudent.com>
2025-08-01 23:37:19 +0200

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""List all cities of a state passed as argument (safe from SQL injection)"""
-
-import sys
+"""
+Lists all cities of a particular state
+"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        exit()
-    user, passwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db)
-    cur = conn.cursor()
-    query = """SELECT cities.name FROM cities
-               JOIN states ON cities.state_id = states.id
-               WHERE states.name = %s
-               ORDER BY cities.id ASC"""
-    cur.execute(query, (state_name,))
-    cities = [row[0] for row in cur.fetchall()]
-    print(", ".join(cities))
-    cur.close()
-    conn.close()
+
+if __name__ == '__main__':
+    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
+    with con.cursor() as cur:
+        """Used context manager to automatically close the cursor object"""
+        cur.execute('''SELECT cities.name FROM cities JOIN states ON
+                    states.id=cities.state_id
+                    WHERE states.name = %s;''', (sys.argv[4], ))
+        print(", ".join([row[0] for row in cur.fetchall()]))
+    con.close()
