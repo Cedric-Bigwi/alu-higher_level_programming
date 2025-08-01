20e3a912b3d129efa6d1c2309b20227f34527e04
Ced <c.bigwihin@alustudent.com>
2025-08-01 23:36:44 +0200

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""List all cities from the database hbtn_0e_4_usa"""
-
-import sys
+"""
+Write a script that lists all states from the database hbtn_0e_4_usa
+"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        exit()
-    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db)
-    cur = conn.cursor()
-    query = """SELECT cities.id, cities.name, states.name 
-               FROM cities 
-               JOIN states ON cities.state_id = states.id 
-               ORDER BY cities.id ASC"""
-    cur.execute(query)
-    for row in cur.fetchall():
-        print(row)
-    cur.close()
-    conn.close()
+
+if __name__ == '__main__':
+    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
+    with con.cursor() as cur:
+        """Used context manager to automatically close the cursor object"""
+        cur.execute('''SELECT cities.id, cities.name, states.name FROM
+                    cities JOIN states ON states.id=cities.state_id;''')
+        [print(row) for row in cur.fetchall()]
+    con.close()
