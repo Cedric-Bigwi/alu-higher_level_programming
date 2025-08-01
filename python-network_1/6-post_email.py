67d09d65526789dc0e4acdd8c34d2c3dd3fc36db
Ced <c.bigwihin@alustudent.com>
2025-08-01 15:10:10 +0200

    Update 6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Sends a POST request with an email parameter and displays the response using requests."""
-
-import requests
+"""__summary__
+- writes a script that takes in a URL and an email address
+- sends a POST request to the URL with the email as a parameter
+- and displays the body of the response (decoded in utf-8)
+"""
 import sys
+import requests
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     url = sys.argv[1]
-    email = sys.argv[2]
-    r = requests.post(url, data={'email': email})
-    print(r.text)
+    value = {'email': sys.argv[2]}
+
+    reqs = requests.post(url, data=value)
+    print(reqs.text)
