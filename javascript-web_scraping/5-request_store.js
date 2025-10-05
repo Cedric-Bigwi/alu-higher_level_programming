6d52a0d4ab6469cd8b234a370d8d7b864c41f432
Ced <c.bigwihin@alustudent.com>
2025-10-05 20:06:07 +0200

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,8 +3,13 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], (err, response, body) => {
-  if (err) console.log(err);
-  else fs.writeFile(process.argv[3], body, 'utf8', err => {
-    if (err) console.log(err);
-  });
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
